1b15921afe20b8cfda764041555a7928e16fd84c
KennethJT1 <112853926+KennethJT1@users.noreply.github.com>
2022-12-23 08:01:40 +0100

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -5,7 +5,7 @@ import {
 } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
 import Menu from "./components/nav/Menu"
-import Home from './pages/auth/Home';
+import Home from './pages/Home';
 import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import Dashboard from './pages/user/Dashboard'
---
+++ client/src/pages/admin/Dasboard.js
@@ -0,0 +1,8 @@
+import {useAuth} from '../../context/auth';
+import Jumbotron from '../../components/cards/Jumbotron';
+
+export default function AdminDasboard() {
+  return (
+    <div><h2>Admin Dashboard</h2></div>
+  )
+}
